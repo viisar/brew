32b2c139cb573364207b33b6fbfedb90651319a7
Dayvid Victor <victor.dvro@gmail.com>
2014-11-14 11:45:04 -0300

    paired metrics added

---
+++ brew/metrics/diversity/paired.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def __get_coefficients(y_true, y_pred_a, y_pred_b):
+    a, b, c, d = 0, 0, 0, 0
+    for i in range(y_true.shape[0]):
+        if y_pred_a[i] == y_true[i] and y_pred_b[i] == y_true[i]:
+            a = a + 1
+        elif y_pred_a[i] != y_true[i] and y_pred_b[i] == y_true[i]:
+            b = b + 1
+        elif y_pred_a[i] == y_true[i] and y_pred_b[i] != y_true[i]:
+            c = c + 1
+        else:
+            d = d + 1
+
+    return a, b, c, d
+
+
+def q_statistics(y_true, y_pred_a, y_pred_b):
+    a, b, c, d = __get_coefficients(y_true, y_pred_a, y_pred_b)
+    q = float(a*d - b*c) / (a*d + b*c)
+    return q
+
+
+def correlation_coefficient_p(y_true, y_pred_a, y_pred_b):
+    a, b, c, d = __get_coefficients(y_true, y_pred_a, y_pred_b)
+    p = float((a*d - b*c)) / np.sqrt((a+b)*(c+d)*(a+c)*(b+d))
+    return p
+
+
+def disagreement_measure(y_true, y_pred_a, y_pred_b):
+    a, b, c, d = __get_coefficients(y_true, y_pred_a, y_pred_b)
+    disagreement = float(b + c) / (a + b + c + d)
+    return disagreement
+    
+
+def agreement_measure(y_true, y_pred_a, y_pred_b):
+    return 1.0/disagreement_measure(y_true, y_pred_a, y_pred_b)
+
+
+def double_fault_measure(y_true, y_pred_a, y_pred_b):
+    a, b, c, d = __get_coefficients(y_true, y_pred_a, y_pred_b)
+    df = float(d) / (a + b + c + d)
+    return df
+    
+
+
+
+
