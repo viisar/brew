cf9e53c0c7d1a2bb6f0279165ee1ef3d47a13f5d
Dayvid Victor <victor.dvro@gmail.com>
2014-11-14 10:52:38 -0300

    package added

---
+++ base.py
@@ -0,0 +1,17 @@
+
+
+
+class EnsembleBase(object):
+
+    def fit(X, y):
+        self.X, self.y = X, y
+
+    def set_validation(X, y):
+        self.vX, self.vy = X, y
+
+    def predict(X):
+        pass
+
+
+
+
