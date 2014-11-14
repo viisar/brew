99bacb7d21643873549e1a8d710bc39dfa1c5a75
thypad <thyago.porpino@gmail.com>
2014-11-14 17:41:28 -0300

    initial version of max rule

--- brew/combination/combiner.py
+++ brew/combination/combiner.py
@@ -1,5 +1,10 @@
 import numpy as np
 
+def max(probs):
+    """ Returns the class label that was chosen with the maximum confidence by any
+    of the classifiers
+    """
+    return probs.max(axis=1).argmax()
 
 def majority_vote(delta):
     """ Returns the class label that had the majority of votes
--- test/test_combiner.py
+++ test/test_combiner.py
@@ -4,23 +4,49 @@ Tests for `brew.combination.combiner` module.
 
 import numpy as np
 from brew.combination.combiner import majority_vote
+from brew.combination.combiner import max
 
 
 class TestMajorityVote():
 
-    # rows are classes, columns are classifiers
+    # rows are classes, columns are classifiers, elements are votes (binary) 
+    # with each classifier (column) limited to one vote, that is example.sum(axis=0)
+    # should be a all-one vector
 
     def test_with_one_classifier_one_class(self):
-        case = np.array([[1]])
-        assert majority_vote(case) == 0
+        example = np.array([[1]])
+        assert majority_vote(example) == 0
     
     def test_with_one_classifier_mult_classes(self):
-        case = np.array([[0],[0],[1]])
-        assert majority_vote(case) == 2
+        example = np.array([[0],[0],[1]])
+        assert majority_vote(example) == 2
 
     def test_with_mult_classifiers_mult_classes(self):
-        delta = np.array( [ [0, 0, 0],
-                            [1, 0, 1],
-                            [0, 1, 0] ] )
-        assert majority_vote(delta) == 1
+        example = np.array( [   [0, 0, 0],
+                                [1, 0, 1],
+                                [0, 1, 0]   ] )
+        assert majority_vote(example) == 1
+
+
+class TestMax():
+
+    # rows are classes, columns are classifiers, elements are posterior probabilities
+    # with each classifier (column) having elements sum to 1, that is example.sum(axis=0)
+    # should be a all-one vector
+
+    def test_with_one_classifier_one_class(self):
+        example = np.array([[1.]])
+        assert max(example) == 0
+    
+    def test_with_one_classifier_mult_classes(self):
+        example = np.array([[0.1],[0.5],[0.4]])
+        assert max(example) == 1
+
+
+    def test_with_mult_classifiers_mult_classes(self):
+        example = np.array( [   [0.3, 0.6, 0.2],
+                                [0.2, 0.2, 0.1],
+                                [0.5, 0.2, 0.7]  ] )
+
+        assert max(example) == 2
 
