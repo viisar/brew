68f764596463f80ea07ab0cc1993bee3ace14748
Dayvid Victor <victor.dvro@gmail.com>
2014-11-24 16:55:06 -0300

    adaptive_bagging.py started

---
+++ brew/generation/adaptive_bagging.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+import sklearn
+from sklearn.lda import LDA
+from sklearn.decomposition import PCA
+
+from brew.base import Ensemble
+from brew.combination.rules import majority_vote_rule
+from brew.combination.combiner import Combiner
+from brew.generation import Bagging
+
+from .base import PoolGenerator
+
+class AdaptiveBagging(PoolGenerator):
+
+    def __init__(self, K=10, n_components=1, base_classifier=None, n_classifiers=100,
+            combination_rule='majority_vote', max_samples=1.0):
+
+        self.K = K
+
+        self.base_classifier = base_classifier
+        self.n_classifiers = n_classifiers
+        self.combination_rule = combination_rule
+        self.bagging = Bagging(base_classifier=base_classifier, n_classifiers=K)
+
+        self.classifiers = None
+        self.ensemble = None
+        self.combiner = Combiner(rule=combination_rule)
+        
+
+    def set_validation(self, X, y):
+        self.validation_X = X
+        self.validation_y = y
+
+
+    def fitness(self, classifier):
+        #TODO implement classifier
+        return np.random.random()
+
+
+    def fit(self, X, y):
+
+        if self.validation_X == None and self.validation_y == None:
+            self.validation_X = X
+            self.validation_y = y
+        
+        classifiers = []     
+    
+        self.bagging.fit(X, y)
+        classifiers.append(np.random.choice(clfs))
+
+        for i in range(self.n_classifiers):
+            #TODO use weights for classes here
+            self.bagging.fit(X, y)
+            clfs = self.bagging.ensemble.classifiers
+            if i == 0:
+                classifiers.append(np.random.choice(clfs))
+            else:
+                mx = -1
+                for idx, clf in enumerate(clfs):
+                    ft = self.fitness(clf)
+                    if ft > idx:
+                        mx = idx
+                classifiers.append(clfs[mx])
+        
+        self.ensemble = Ensemble()
+        self.ensemble.add_classifiers(classifiers)
+        return self
+
+    def predict(self, X):
+        out = self.ensemble.output(X)
+        return self.combiner.combine(out)
+            
