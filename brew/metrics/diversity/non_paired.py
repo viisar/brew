819558615c12ecd437d54c8c57b1c232ec566c3d
Dayvid Victor <victor.dvro@gmail.com>
2015-01-20 16:50:37 -0300

    non_paired fixed

--- brew/metrics/diversity/non_paired.py
+++ brew/metrics/diversity/non_paired.py
@@ -2,6 +2,7 @@ import numpy as np
 
 def entropy_measure_e(ensemble, X, y):
     factor = 0
+    print len(ensemble)
     for j in range(y.shape[0]):
         right, wrong = 0, 0
         for estimator in ensemble.classifiers:
@@ -14,6 +15,7 @@ def entropy_measure_e(ensemble, X, y):
         factor = factor + min(right, wrong)
 
     e = (1.0/len(X)) * (1.0 / (len(ensemble) - len(ensemble)/2)) * factor
+    
     return e
 
 def kohavi_wolpert_variance(ensemble, X, y):
