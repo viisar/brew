1e57f728df3b20b993fec3cf5d3e001ef585e440
Dayvid Victor <victor.dvro@gmail.com>
2015-03-13 18:04:03 -0300

    KNORA E test ok

--- test/test_selection_dynamic.py
+++ test/test_selection_dynamic.py
@@ -1,5 +1,5 @@
 """
-Tests for `brew.base` module.  """
+Tests for `brew.selection.dynamic` module.  """
 
 import numpy as np
 
@@ -33,11 +33,62 @@ Xtra, Xval, ytra, yval = train_test_split(Xtra, ytra, test_size=0.30)
 bag = Bagging(base_classifier=DecisionTreeClassifier(), n_classifiers=5)
 bag.fit(Xtra, ytra)
 
+class KNORA_UNION_2(KNORA):
+    
+    def select(self, ensemble, x):
+        neighbors_X, neighbors_y = self.get_neighbors(x)
+       
+        pool = []
+        for i, neighbor in enumerate(neighbors_X):
+            for c in ensemble.classifiers:
+                if c.predict(neighbor) == neighbors_y[i]:
+                    pool.append(c)
+
+        return Ensemble(classifiers=pool), None
+
+
+
+class KNORA_ELIMINATE_2(KNORA):
+    def select(self, ensemble, x):
+        neighbors_X, neighbors_y = self.get_neighbors(x)
+
+        k = self.K
+
+        pool = []
+        while k > 0:
+            nn_X = neighbors_X[:k,:]
+            nn_y = neighbors_y[:k]
+
+            for i, c in enumerate(ensemble.classifiers):
+                if np.all(c.predict(nn_X) == nn_y[np.newaxis, :]):
+                    pool.append(c)
+
+            if not pool: # empty
+                k = k-1
+            else:
+                break
+
+        if not pool: # still empty
+            # select the classifier that recognizes
+            # more samples in the whole neighborhood
+            # also select classifiers that recognize
+            # the same number of neighbors
+            pool = self._get_best_classifiers(ensemble, neighbors_X, neighbors_y, x)
+
+
+        return Ensemble(classifiers=pool), None
+
 class TestKNORA_E():
     def test_simple(self):
-        selector = KNORA_ELIMINATE(Xval=Xval, yval=yval)
-        for x in Xtst:
-            pool, w = selector.select(bag.ensemble, x)
+        selector_pred = KNORA_ELIMINATE(Xval=Xval, yval=yval)
+        selector_true = KNORA_ELIMINATE_2(Xval=Xval, yval=yval)
+        for x in Xtst[:10]:
+            pool_pred, w_pred = selector_pred.select(bag.ensemble, x)
+            pool_true, w_true = selector_true.select(bag.ensemble, x)
+            assert w_pred == w_true
+            assert len(pool_pred) == len(pool_true)
+            for c_p, c_t in zip(pool_pred.classifiers, pool_true.classifiers):
+                assert c_p == c_t
 
 
 class TestKNORA_U():
