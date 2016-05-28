fdd6e726db57d25509d046fe4e3f2d65ed0e528e
Dayvid Victor <victor.dvro@gmail.com>
2016-05-28 10:13:51 -0300

    brew 0.1.3

--- brew/__init__.py
+++ brew/__init__.py
@@ -1,3 +1,3 @@
 __author__ = 'Dayvid Victor <victor.dvro@gmail.com>, Thyago Porpino <thyago.porpino@gmail.com>'
 __email__='brew-python-devs@googlegroups.com',
-__version__ = '0.1.2'
+__version__ = '0.1.3'
