1d49d311d6e2b8d61154625e6499acc7357e3b69
Dayvid Victor <victor.dvro@gmail.com>
2016-07-13 15:17:20 -0300

    starting selection.dynamic refactoring

--- brew/selection/dynamic/base.py
+++ brew/selection/dynamic/base.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-from sklearn.ensemble import BaggingClassifier
-from sklearn.neighbors.classification import KNeighborsClassifier
-
 from abc import abstractmethod
+from sklearn.neighbors import NearestNeighbors
+
+from .region_of_competence import RegionOfCompetenceSelector
 
 class DCS(object):
 
@@ -11,34 +11,58 @@ class DCS(object):
     def select(self, ensemble, x):
         pass
 
-    def __init__(self, Xval, yval, K=5, weighted=False, knn=None):
-        self.Xval = Xval
-        self.yval = yval
-        self.K = K
+    def __init__(self, X_val, y_val, roc_sel_type='neighbors', n_neighbors=7, radius=1.0, **kwargs):
+        self.X_val = X_val
+        self.y_val = y_val
 
-        if knn == None:
-            self.knn = KNeighborsClassifier(n_neighbors=K, algorithm='brute')
+        roc_sel_type_list = ('neighbors', 'radius', 'custom')
+        if roc_sel_type not in roc_sel_type_list:
+            raise NotImplementedError
+
+        if roc_sel_type == 'custom':
+            if 'roc_sel' in kwargs:
+                roc_sel = kwargs['roc_sel']
+            else:
+                raise Exception('roc_sel_type=\'custom\' requires a \'roc_sel\' argument')
         else:
-            self.knn = knn
+            self.roc_sel = NearestNeighbors(n_neighbors=n_neighbors, radius=radius, **kwargs)
 
-        self.knn.fit(Xval, yval)
-        self.weighted = weighted
+        self.n_neighbors = n_neighbors
+        self.radius = radius
 
-
-    def get_neighbors(self, x, return_distance=False):
+    def region_of_competence(self, x, delimiter=None, return_distance=False):
         # obtain the K nearest neighbors of test sample in the validation set
-        if not return_distance:
-            [idx] = self.knn.kneighbors(x, 
-                    return_distance=return_distance)
+        if delimiter is None:
+            delimiter = self.radius if self.roc_sel_type == 'radius' else self.n_neighbors
+
+        if self.roc_sel_type == 'neighbors':
+            out = self.roc_sel.kneighbors(x.reshape(-1,1), n_neighbors=delimiter, return_distance=return_distance)
+        elif self.roc_sel_type == 'radius':
+            out = self.roc_sel.radius_neighbors(x.reshape(-1,1), radius=delimiter, return_distance=return_distance)
+
+        if return_distance:
+            [dists], [idx] = out
         else:
-            [dists], [idx] = self.knn.kneighbors(x, 
-                    return_distance=return_distance)
-        X_nn = self.Xval[idx] # k neighbors
-        y_nn = self.yval[idx] # k neighbors target
+            [idx] = out
+
+        X_nn = self.X_val[idx] # k neighbors
+        y_nn = self.y_val[idx] # k neighbors target
 
         if return_distance:
             return X_nn, y_nn, dists
         else:
             return X_nn, y_nn
 
+    def region_of_competence_idx(self, x, delimiter=None):
+        # obtain the K nearest neighbors of test sample in the validation set
+        if delimiter is None:
+            delimiter = self.radius if self.roc_sel_type == 'radius' else self.n_neighbors
+
+        if self.roc_sel_type == 'neighbors':
+            [idx] = self.roc_sel.kneighbors(x.reshape(-1,1), n_neighbors=delimiter, return_distance=False)
+        elif self.roc_sel_type == 'radius':
+            [idx] = self.roc_sel.radius_neighbors(x.reshape(-1,1), radius=delimiter, return_distance=False)
+
+
+        return idx
 
