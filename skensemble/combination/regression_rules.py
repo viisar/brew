eb09a640282c2832bb549873697f749ce9535cb9
Thyago Porpino <thyago.porpino@gmail.com>
2016-11-28 09:20:33 -0300

    improving documentation of regression rules

--- skensemble/combination/regression_rules.py
+++ skensemble/combination/regression_rules.py
@@ -3,72 +3,65 @@
 import numpy as np
 
 
-def max_rule(probs):
-    """ Implements the max rule as defined by [1].
-
-    This rule only makes sense if the classifiers output
-    the posterior probabilities for each class.
-
-    Parameters
-    ----------
-    probs:  Numpy 2d-array with rows representing each class, columns
-            representing each classifier and elements representing
-            posterior probabilities. Each column should sum up to
-            one as a sanity check that the probabilities are valid.
-    """
-
-    return probs.max(axis=1)
+RULE_FUNCTIONS = {
+    'max': max_rule,
+    'min': min_rule,
+    'mean': mean_rule,
+    'median': median_rule
+}
 
 
-def min_rule(probs):
-    """ Implements the min rule as defined by [1].
-
-    This rule only makes sense if the classifiers output
-    the posterior probabilities for each class.
+def max_rule(array):
+    """ Outputs the maximum output of all the regressors.
 
     Parameters
     ----------
-    probs:  Numpy 2d-array with rows representing each class, columns
-            representing each classifier and elements representing
-            posterior probabilities. Each column should sum up to
-            one as a sanity check that the probabilities are valid.
+    array:  Numpy 1d-array with elements representing each
+            regressor output.
+
+    returns The maximum value output by any of the regressors.
     """
 
-    return probs.min(axis=1)
+    return array.max(axis=1)
 
 
-def mean_rule(probs):
-    """
-    Implements the first case of the median rule as defined by [1].
-
-    This rule only makes sense if the classifiers output
-    the posterior probabilities for each class.
+def min_rule(array):
+    """ Outputs the maximum output of all the regressors.
 
     Parameters
     ----------
-    probs:  Numpy 2d-array with rows representing each class, columns
-            representing each classifier and elements representing
-            posterior probabilities. Each column should sum up to
-            one as a sanity check that the probabilities are valid.
+    array:  Numpy 1d-array with elements representing each
+            regressor output.
+
+    returns The maximum value output by any of the regressors.
     """
 
-    return probs.mean(axis=1)
+    return array.min(axis=1)
 
 
-def median_rule(probs):
-    """
-    Implements the second case of the median rule as defined by [1].
-
-    This rule only makes sense if the classifiers output
-    the posterior probabilities for each class.
+def mean_rule(array):
+    """ Outputs the maximum output of all the regressors.
 
     Parameters
     ----------
-    probs:  Numpy 2d-array with rows representing each class, columns
-            representing each classifier and elements representing
-            posterior probabilities. Each column should sum up to
-            one as a sanity check that the probabilities are valid.
+    array:  Numpy 1d-array with elements representing each
+            regressor output.
+
+    returns The maximum value output by any of the regressors.
     """
 
-    # numpy array has no median method
-    return np.median(probs, axis=1)
+    return array.mean(axis=1)
+
+
+def median_rule(array):
+    """ Outputs the maximum output of all the regressors.
+
+    Parameters
+    ----------
+    array:  Numpy 1d-array with elements representing each
+            regressor output.
+
+    returns The maximum value output by any of the regressors.
+    """
+
+    return np.median(array, axis=1)
