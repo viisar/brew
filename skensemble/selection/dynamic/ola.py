b9d05e6f88bc465093df091f5f79836b31303c72
Dayvid Victor <victor.dvro@gmail.com>
2016-12-25 13:19:40 -0300

    refactoring dynamic selection methods

--- skensemble/selection/dynamic/ola.py
+++ skensemble/selection/dynamic/ola.py
@@ -1,10 +1,15 @@
 import numpy as np
 
-from brew.base import Ensemble
+from collections import Counter
+
+from skensemble.ensemble import Ensemble, output2labels
+from skensemble.utils import has_consensus, get_consensus, get_ties
+
 from .base import DCS
 
+VALID_TIE_MODES = ['ignore', 'break']
 
-class OLA2(DCS):
+class OLA(DCS):
     """Overall Local Accuracy.
 
     The Overall Local Accuracy selects the best classifier for
@@ -12,41 +17,31 @@ class OLA2(DCS):
 
     Attributes
     ----------
-    `Xval` : array-like, shape = [indeterminated, n_features]
-        Validation set.
+    Xval : array-like, shape (n_samples, n_features)
+        Samples of the validation set.
 
-    `yval` : array-like, shape = [indeterminated]
+    yval : array-like, shape (n_samples)
         Labels of the validation set.
 
-    `knn`  : sklearn KNeighborsClassifier,
-        Classifier used to find neighborhood.
+    roc_selector : estimator, optional (default = KNeighborsClassifier)
+        Estimator used to select the region of competence of the test samples.
+        Must implement the kneighbors method. Usually, the estimator used is
+        the KNeighborsClassifier from scikit-learn.
 
+    roc_size : int, size of the region of competence, optional (default = 7)
+        The number of neighbors used when selecting the region of competence of
+        the test sample. Depending on the roc_selector used, roc_size might be
+        ignored.
 
-    Examples
-    --------
-    >>> from brew.selection.dynamic.ola import OLA
-    >>> from brew.generation.bagging import Bagging
-    >>> from brew.base import EnsembleClassifier
-    >>>
-    >>> from sklearn.tree import DecisionTreeClassifier
-    >>> import numpy as np
-    >>>
-    >>> X = np.array([[-1, 0], [-0.8, 1], [-0.8, -1], [-0.5, 0], [0.5, 0],
-                      [1, 0], [0.8, 1], [0.8, -1]])
-    >>> y = np.array([1, 1, 1, 2, 1, 2, 2, 2])
-    >>> tree = DecisionTreeClassifier(max_depth=1, min_samples_leaf=1)
-    >>> bag = Bagging(base_classifier=tree, n_classifiers=10)
-    >>> bag.fit(X, y)
-    >>>
-    >>> ola = OLA(X, y, K=3)
-    >>>
-    >>> clf = EnsembleClassifier(bag.ensemble, selector=ola)
-    >>> clf.predict([-1.1,-0.5])
-    [1]
+    tie_mode : str, how the competence ties are handled, optional (default = 'ignore')
+        - If 'ignore', randomly selects one of the  most competent estimators.
+        - If 'break', majority vote is used to break ties.
+
+        NOTE: 'ignore' is faster.
 
     See also
     --------
-    brew.selection.dynamic.lca.LCA: Local Class Accuracy.
+    skensemble.selection.dynamic.lca.LCA: Local Class Accuracy.
 
     References
     ----------
@@ -59,62 +54,52 @@ class OLA2(DCS):
     "From dynamic classifier selection to dynamic ensemble selection."
     Pattern Recognition 41.5 (2008): 1718-1731.
     """
+    def __init__(self, roc_selector=None, roc_size=7, tie_mode='ignore'):
+        super(OLA, self).__init__(roc_selector, roc_size)
 
-    def select(self, ensemble, x):
-        if ensemble.in_agreement(x):
-            return Ensemble([ensemble.classifiers[0]]), None
+        if tie_mode not in VALID_TIE_MODES:
+            raise NotImplementedError
 
-        # intialize variables
-        # the the indexes of the KNN of x
-        classifiers = ensemble.classifiers
-        [idx] = self.knn.kneighbors(x, return_distance=False)
-        X, y = self.Xval[idx], self.yval[idx]
+        self.tie_mode = tie_mode
 
-        # d[score] = indexes of the classifiers with that score
-        d = {}
-        scores = [clf.score(X, y) for clf in ensemble.classifiers]
-        for i, scr in enumerate(scores):
-            d[scr] = d[scr] + [i] if scr in d else [i]
-        best_scores = sorted([k for k in d.iterkeys()], reverse=True)
+    def _select(self, ensemble, x):
+        X, y = self.get_roc(x, return_distance=False)
 
-        # if there was a single best classifier, return it
-        if len(d[best_scores[0]]) == 1:
-            i = d[best_scores[0]][0]
-            return Ensemble([classifiers[i]]), None
+        output = ensemble.output(X)
+        labels = output2labels(output, classes=ensemble.classes_)
+        # labels shape (n_samples, n_estimators)
 
-        options = None
-        for j, score in enumerate(best_scores):
-            pred = [classifiers[index].predict(x) for index in d[score]]
-            pred = np.asarray(pred).flatten()
+        scores = np.sum(labels == y[:,np.newaxis], axis=0).ravel()
+        assert len(scores) == len(ensemble)
 
-            bincount = np.bincount(pred.astype(int))
-            if options is not None:
-                for i in range(len(bincount)):
-                    bincount[i] = bincount[i] if i in options else 0
+        if self.tie_mode == 'ignore':
+            best_estimator = ensemble._estimators[np.argmax(scores)]
+            return Ensemble([best_estimator]), None
 
-            imx = np.argmax(bincount)
-            votes = np.argwhere(bincount == bincount[imx]).flatten()
-            count = len(votes)
-            if count == 1:
-                return Ensemble([classifiers[np.argmax(pred == imx)]]), None
-            elif options is None:
-                options = votes
+        elif self.tie_mode == 'break':
+            options = None
 
-        return Ensemble([classifiers[np.argmax(scores)]]), None
+            test_labels = output2labels(ensemble.output(x.reshape(1,-1)),
+                    ensemble.classes_).ravel()
+
+            [high_score_idx] = np.where(scores == np.max(scores))
+
+            for i, score_level in enumerate(sorted(set(score), reverse=True)):
+                [idx] = np.where(scores == score_level)
+                consensus = get_consensus(test_labels[idx], options)
+                if consensus is not None:
+                    [c_idx] = np.where(test_labels[high_score_idx] == consensus)
+                    best_estimator_idx = high_score_idx[c_idx[0]]
+                    best_estimator = ensemble._estimators[best_estimator_idx]
+                    return Ensemble([best_estimator])
+                elif options is None:
+                    options = set(test_labels[idx])
+                else:
+                    ties = get_ties(test_labels[idx], options=options)
+                    options = options.union(set(ties))
+
+            # if has not selected any use random selection criterion
+            best_estimator = ensemble._estimators[np.argmax(scores)]
+            return Ensemble([best_estimator]), None
 
 
-class OLA(DCS):
-
-    def select(self, ensemble, x):
-        if ensemble.in_agreement(x):
-            return Ensemble([ensemble.classifiers[0]]), None
-
-        # intialize variables
-        # the the indexes of the KNN of x
-        classifiers = ensemble.classifiers
-        [idx] = self.knn.kneighbors(x, return_distance=False)
-        X, y = self.Xval[idx], self.yval[idx]
-
-        scores = np.asarray([clf.score(X, y) for clf in classifiers])
-
-        return Ensemble([classifiers[np.argmax(scores)]]), None
